e098ec4051cd240602ca5f36f9aa248f9f10afc6
Jaime <jaimenovillobenito@gmail.com>
2024-08-21 21:56:50 +0200

    Redirect URI updated

--- src/Auth.js
+++ src/Auth.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CLIENT_ID = "62c10705764347f589fd879a596f5e2a";
-const REDIRECT_URI = `${window.location.origin}`;
+const REDIRECT_URI = `https://spotify-project-jaime-novillo.netlify.app/`;
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=playlist-modify-public playlist-modify-private`;
 
 function Auth() {
