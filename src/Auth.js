bfec21c47f451c497e9e1eb7c118cd48d2b1b54c
Jaime <jaimenovillobenito@gmail.com>
2024-08-21 21:48:14 +0200

    Auth styles added

--- src/Auth.js
+++ src/Auth.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const CLIENT_ID = "62c10705764347f589fd879a596f5e2a";
-const REDIRECT_URI = `${window.location.origin}`; 
+const REDIRECT_URI = `${window.location.origin}`;
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=playlist-modify-public playlist-modify-private`;
 
 function Auth() {
   return (
-    <div id='signIn'>
-      <a href={AUTH_URL}>Iniciar sesión con Spotify</a>
+    <div id="signIn">
+      <a href={AUTH_URL}>
+        <p>Iniciar sesión con Spotify</p>
+        <img src="/images/spotify.svg" alt="Spotify logo" />
+      </a>
     </div>
   );
 }
